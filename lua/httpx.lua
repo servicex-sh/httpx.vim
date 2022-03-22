bb54e4f8aad73c6ae788b29be05287bd42beb28a
linux_china <libing.chen@gmail.com>
2022-03-21 17:08:10 -0700

    httpx lua module

---
+++ lua/httpx.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+function M.execute()
+    local filePath = vim.api.nvim_buf_get_name(0)
+    local lineNum = vim.api.nvim_win_get_cursor(0)[1]
+    local cmd = "httpx -f " .. filePath .. " line:" .. lineNum
+    local output = vim.fn.system(cmd)
+    print(output)
+end
+
+return M
