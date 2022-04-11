62bbf0a952f1b9dc4f1768f512a0edd4b2a528a5
linux_china <libing.chen@gmail.com>
2022-04-10 19:49:58 -0700

    execute write command to run target

--- lua/httpx.lua
+++ lua/httpx.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.execute()
+    vim.api.nvim_command('write')
     local filePath = vim.api.nvim_buf_get_name(0)
     local lineNum = vim.api.nvim_win_get_cursor(0)[1]
     local cmd = "httpx -f " .. filePath .. " line:" .. lineNum
